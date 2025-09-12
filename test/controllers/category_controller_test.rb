2f7e9dc4597bbdd6bc1f9f3b05dbd90597c07d64
Karla Patricia Farinas <karlafarinas@gmail.com>
2025-09-12 19:17:56 +0800

    Fixed some details in production of database.yml also added model test for category.

--- test/controllers/category_controller_test.rb
+++ test/controllers/category_controller_test.rb
@@ -1,7 +1,58 @@
 require "test_helper"
 
-class CategoryControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in @user
+    @category = categories(:one)
+  end
+
+  test "should get index" do
+    get categories_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_category_url
+    assert_response :success
+  end
+
+  test "should create category" do
+    assert_difference("Category.count") do
+      post categories_url, params: { category: { name: "New category" } }
+    end
+    assert_redirected_to category_url(Category.last)
+  end
+
+  # test "should not create category with invalid data" do
+  #   assert_no_difference("Category.count") do
+  #     post categories_url, params: { category: { name: "" } }
+  #   end
+  #   assert_response :success
+  #   assert_template "categories/new"
   # end
+
+  test "should show category" do
+    get category_url(@category)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_category_url(@category)
+    assert_response :success
+  end
+
+  test "should update category" do
+    patch category_url(@category), params: { category: { name: "Updated category" } }
+    assert_redirected_to category_url(@category)
+    @category.reload
+    assert_equal "Updated category", @category.name
+  end
+
+  test "should destroy category" do
+    assert_difference("Category.count", -1) do
+      delete category_url(@category)
+    end
+    assert_redirected_to categories_url
+  end
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name must be present" do
+    category = Category.new(name: "")
+    assert !category.valid?, "Category should have a name"
+  end
+
+  test "category belongs to user" do
+    category = Category.new
+    assert_respond_to category, :user
+  end
+
+  test "category has many tasks" do
+    category = Category.new
+    assert_respond_to category, :tasks
+  end
+
+  test "destroying category destroys associated tasks" do
+    category = categories(:one)
+    task_count = category.tasks.count
+    
+    assert_difference("Task.count", -task_count) do
+      category.destroy
+    end
+  end
 end
